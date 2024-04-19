789cd2b3dfd67492a58190861dcb9a0f635d5d86
Abdul Rehman <64700510+abdulrehmancom33@users.noreply.github.com>
2024-04-19 20:54:22 +0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -32,8 +32,6 @@ urlpatterns = [
     # UPLOAD IMAGE API
     path('uploadimage/',views.UploadViewSet.as_view(), name='uploads'),
 
-
-
     # NON USED/ONLY FOR TESTING APIS
     # path('login/', obtain_auth_token, name='api_token_auth'),
     # path('Dogs/',views.DogListView.as_view(), name='dog'),
